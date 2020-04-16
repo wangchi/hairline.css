730ab71de6fd2f81f1743edeeae666a9118d7751
Wang Chi <hiwangchi@gmail.com>
2020-04-17 01:04:32 +0800

    add example

---
+++ precache-manifest.dd20a0d90ea38c203ef6034962c3ba0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7e2e2afc282aef5789b8594ba101ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09fbc15ca2f2c41fd20",
+    "url": "/static/css/2.05900efb.chunk.css"
+  },
+  {
+    "revision": "bf93ef2c83541bbf8fa9",
+    "url": "/static/css/main.b6b6ba64.chunk.css"
+  },
+  {
+    "revision": "b09fbc15ca2f2c41fd20",
+    "url": "/static/js/2.38fcac05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38fcac05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf93ef2c83541bbf8fa9",
+    "url": "/static/js/main.78ccddc1.chunk.js"
+  },
+  {
+    "revision": "790698d0307bb52109e4",
+    "url": "/static/js/runtime-main.3a55caf4.js"
+  }
+]);
